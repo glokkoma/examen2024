e274b11c6fd9a8cbb908f0fd699ee1f4380c2b00
Jesús De Federico Bedoya <jesus.s440560@cesurformacion.com>
2024-11-22 10:18:14 +0100

    archivo php y css

---
+++ jesusdefedericobedoya/entorno.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Jesús de Federico Bedoya</h1>
+    <?php
+    echo "<h2> Examen Entorno de Desarrollo.Suerte!</h2>";
+    ?>
+</body>
+</html>
